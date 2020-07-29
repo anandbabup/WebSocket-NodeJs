c69fde24f726c95c14d341b70a6ec0726bd5c082
anandbabup <anand.net@gmail.com>
2020-07-29 20:49:45 +0530

    Loading 89k records in 2secs.

--- client/index.js
+++ client/index.js
@@ -4,37 +4,38 @@ window.onload = () => {
     const socketConnection = new WebSocket('ws://www.localhost:3000/');
     // when connection opens, log it to the console
     socketConnection.onopen = (connectionEvent) => {
-      console.log('websocket connection is open', connectionEvent);
+        console.log('websocket connection is open', connectionEvent);
     };
-    
+
     // when a message is received from the socket connection,
     // the message will contain the id of a button that the other player clicked
     socketConnection.onmessage = (messageObject) => {
-      // if the button is unclicked, changes its text to "O", and disable the button
-      const buttonClicked = document.getElementById(messageObject.data);
-      if (buttonClicked.innerHTML === '-') {
-        buttonClicked.innerHTML = 'O';
-        buttonClicked.disabled = true;
-      }
+        console.log(messageObject.data);
+        //   // if the button is unclicked, changes its text to "O", and disable the button
+        //   const buttonClicked = document.getElementById(messageObject.data);
+        //   if (buttonClicked.innerHTML === '-') {
+        //     buttonClicked.innerHTML = 'O';
+        //     buttonClicked.disabled = true;
+        //   }
     };
-    
+
     // an event listener to log any errors with the socket connection.
     socketConnection.onerror = (error) => {
-      console.log('socket error: ', error);
+        console.log('socket error: ', error);
     };
-    
+
     // put an event listener on every button that changes the text to "X",
     // disables the button, and sends a message through the socket connection
     // with the id of the clicked button
     const collectionOfButtons = document.querySelectorAll('button');
     collectionOfButtons.forEach((buttonElement) => {
-      buttonElement.addEventListener('click', (event) => {
-        // set the target's value to "X" and disable the button
-        event.target.innerHTML = 'X';
-        event.target.disabled = true;
-    
-        // send message (of the clicked button's id) through the socket connection
-        socketConnection.send(event.target.id);
-      });
+        buttonElement.addEventListener('click', (event) => {
+            // set the target's value to "X" and disable the button
+            event.target.innerHTML = 'X';
+            event.target.disabled = true;
+
+            // send message (of the clicked button's id) through the socket connection
+            socketConnection.send(event.target.id);
+        });
     });
-   };
+};
--- server/server.js
+++ server/server.js
@@ -3,34 +3,83 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const WebSocket = require('ws');
- 
+//const csv = require('csv-parser');
+
+const fs = require('fs');
+const csv = require('csvtojson')
+const csvFilePath = 'SON_TGSAR_source.csv'
+
 const app = express();
 const server = http.createServer(app);
 const wsServer = new WebSocket.Server({ server });
- 
+
 // a set to hold all of our connected clients
 const setClients = new Set();
- 
+
 wsServer.on('connection', (socketConnection) => {
- // When a connection opens, add it to the clients set and log the number of connections
- setClients.add(socketConnection);
- console.log('New client connected, total connections is: ', setClients.size);
- 
- // When the client sends a message to the server, relay that message to all clients
- socketConnection.on('message', (message) => {
-   setClients.forEach((oneClient) => {
-     oneClient.send(message);
-   });
- });
- 
- // When a connection closes, remove it from the clients set and log the number of connections
- socketConnection.on('close', () => {
-   setClients.delete(socketConnection);
-   console.log('Client disconnected, total connections is: ', setClients.size);
- });
+    // When a connection opens, add it to the clients set and log the number of connections
+    setClients.add(socketConnection);
+    console.log('New client connected, total connections is: ', setClients.size);
+
+    // When the client sends a message to the server, relay that message to all clients
+    socketConnection.on('message', (message) => {
+        //get csv file from server
+        csv()
+            .fromFile(csvFilePath)
+            .then((jsonObj) => {
+                setClients.forEach((oneClient) => {
+                    oneClient.send(JSON.stringify(jsonObj));
+                });
+                //console.log(jsonObj);
+                /**
+                 * [
+                 * 	{a:"1", b:"2", c:"3"},
+                 * 	{a:"4", b:"5". c:"6"}
+                 * ]
+                 */
+            })
+
+    });
+
+    // When a connection closes, remove it from the clients set and log the number of connections
+    socketConnection.on('close', () => {
+        setClients.delete(socketConnection);
+        console.log('Client disconnected, total connections is: ', setClients.size);
+    });
+
+
 });
- 
+
 // serve up static files
 app.use(express.static(path.resolve(__dirname, '../client')));
- 
+
+
+// //Read csv file about 3MB
+// fs.createReadStream('SON_TGSAR.csv')
+//   .pipe(csv())
+//   .on('data', (row) => {
+//     console.log(row);
+//   })
+//   .on('end', () => {
+//     console.log('CSV file successfully processed');
+//   });
+
+
+
+// csv()
+//     .fromFile(csvFilePath)
+//     .then((jsonObj) => {
+//         console.log(jsonObj);
+//         /**
+//          * [
+//          * 	{a:"1", b:"2", c:"3"},
+//          * 	{a:"4", b:"5". c:"6"}
+//          * ]
+//          */
+//     })
+
+// Async / await usage
+
+//const jsonArray = await csv().fromFile(csvFilePath);
+
 server.listen(3000, () => { console.log('listening on port 3000'); });
